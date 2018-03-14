ab4443293326a4ed0d7730b2abfebcfc6b493e76
Julian F. Nunez <julian.dia3@gmail.com>
2018-03-14 17:49:44 -0400

    Triggering correct deployments.

--- src/main/java/com/hdsupply/xmi/config/AppConfig.java
+++ src/main/java/com/hdsupply/xmi/config/AppConfig.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -18,16 +17,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @ComponentScan("com.hdsupply.xmi")
 public class AppConfig implements WebMvcConfigurer {
 	
-	@Bean	
-	public ThreadPoolTaskExecutor taskExecutorKinesis() {
-		ThreadPoolTaskExecutor pool = new ThreadPoolTaskExecutor();
-		pool.setCorePoolSize(5);
-		pool.setMaxPoolSize(10);
-		pool.setAwaitTerminationSeconds(5);
-		pool.setWaitForTasksToCompleteOnShutdown(true);
-		return pool;
-	}	
-
     @Bean
     public CacheManager cacheManager() {
         return new ConcurrentMapCacheManager("policies", "bluFis", "beacons");
