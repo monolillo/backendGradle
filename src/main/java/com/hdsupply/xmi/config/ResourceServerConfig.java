acfef1298fc5689f965f02eaf78abd004156eec4
julian.nunez <julian.nunez@JULIANNUNEZ1.neoris.cxnetworks.net>
2018-03-20 15:00:34 -0400

    Initial implementation of JWT security.

---
+++ src/main/java/com/hdsupply/xmi/config/ResourceServerConfig.java
@@ -0,0 +1,54 @@
+package com.hdsupply.xmi.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
+
+@Configuration
+@EnableResourceServer
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+    @Autowired
+    private ResourceServerTokenServices tokenServices;
+
+    @Value("${security.jwt.resource-ids}")
+    private String resourceIds;
+    
+    @Autowired
+    private DataSource datasource;
+
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+        resources.resourceId(resourceIds).tokenServices(tokenServices);
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+                http
+                .requestMatchers()
+                .and()
+                .authorizeRequests()
+                .antMatchers("/actuator/**", "/api-docs/**").permitAll()
+                .antMatchers("/springjwt/**" ).authenticated();
+    }
+    
+    @Bean
+    public UserDetailsService jdbcDaoImpl() {
+    	
+    	JdbcDaoImpl jdbcDaoImpl = new JdbcDaoImpl();
+    	jdbcDaoImpl.setEnableGroups(true);
+    	jdbcDaoImpl.setEnableAuthorities(false);
+    	jdbcDaoImpl.setDataSource(datasource);
+    	
+    	return jdbcDaoImpl;
+    }
+}
