ec6a76b7aeff159d100bcf95b359aadddf9b8c93
Julian F. Nunez <julian.nunez@neoris.com>
2018-03-21 13:13:19 -0400

    Removed unused import.

--- src/main/java/com/hdsupply/xmi/config/SecurityConfig.java
+++ src/main/java/com/hdsupply/xmi/config/SecurityConfig.java
@@ -13,7 +13,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
