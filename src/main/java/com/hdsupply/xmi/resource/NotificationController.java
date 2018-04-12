1bb49c25187bbb4efa071cf9d4c2050ad9f7433d
Rodrigo Masanes <rodrigo.masanes@neoris.com>
2018-04-12 17:46:24 -0300

    Partial commit US-4.1: Show All Notifications for Consumables with ability to Filter by All, Critical, Non-critical

--- src/main/java/com/hdsupply/xmi/resource/NotificationController.java
+++ src/main/java/com/hdsupply/xmi/resource/NotificationController.java
@@ -2,17 +2,20 @@ package com.hdsupply.xmi.resource;
 
 import java.util.List;
 
+import javax.websocket.server.PathParam;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.hdsupply.xmi.domain.FilterNotification;
 import com.hdsupply.xmi.domain.ProductCatalog;
 import com.hdsupply.xmi.service.NotificationService;
 
+@RestController
 public class NotificationController extends BaseRestController {
 	
 	@Autowired
@@ -20,9 +23,11 @@ public class NotificationController extends BaseRestController {
 	
 	@RequestMapping(value="/site/{siteId}/product/notification", method=RequestMethod.GET)
 	@PreAuthorize("hasAuthority('NOTIFICATION')")
-	public List<ProductCatalog> getNotification(@RequestBody FilterNotification filter, @PathVariable("siteId") Integer siteId) {
+	public List<ProductCatalog> getNotification(@PathParam("critical") Boolean critical, @PathVariable("siteId") Integer siteId) {
 		
+		FilterNotification filter = new FilterNotification();
 		filter.setSiteId(siteId);
+		filter.setCritical(critical);
 		
 		List<ProductCatalog> productCatalogList = notificationService.getNotification(filter);
 		
